ebe50af302babc6e429e5147c3dc01ac4262d946
vishnu856 <vishnunaray@gmail.com>
2017-12-26 14:40:13 -0700

    Added more options to experiment.

--- readin/models.py
+++ readin/models.py
@@ -8,10 +8,17 @@ def user_directory_path(instance, filename):
 	return 'users/{0}'.format(filename)
 
 class Post(models.Model):
+	ALGO_CHOICES=(
+		('S','Supervised'),
+		('U','Unsupervised'),
+	)
+	title=models.CharField(max_length=200, default="Untitled")
 	inputfile=models.FileField(upload_to=user_directory_path, null=True)
-	
+	comments=models.CharField(max_length=500, null=True)
+	algorithm_choice=models.CharField(max_length=2, choices=ALGO_CHOICES, default='S')
+
 	def get_absolute_url(self):
 		return reverse('home')
 
 	def __str__(self):
-		return basename(self.inputfile.name)
+		return basename(self.inputfile.title)
